a6f0be6d9a29588398aba5872f2b23a5c7729d07
Sergei Khaletski <sergei.khaletski@gmail.com>
2021-01-19 11:26:11 +0300

    мой первый проект

---
+++ src/eu/senla/Task1/GetSet.java
@@ -0,0 +1,61 @@
+package eu.senla.Task1;
+
+public class GetSet {
+
+
+        private byte a;
+        private short c;
+        private float g;
+        private double h;
+        private char j;
+        private boolean k;
+
+
+        public byte getA () {
+            return a;
+        }
+
+        public void setA ( byte a){
+            this.a = a;
+        }
+
+        public short getC () {
+            return c;
+        }
+
+        public void setC ( short c){
+            this.c = c;
+        }
+
+    public float getG() {
+        return g;
+    }
+
+    public void setG(float g) {
+        this.g = g;
+    }
+
+    public double getH() {
+        return h;
+    }
+
+    public void setH(double h) {
+        this.h = h;
+    }
+
+    public char getJ() {
+        return j;
+    }
+
+    public void setJ(char j) {
+        this.j = j;
+    }
+
+    public boolean isK() {
+        return k;
+    }
+
+    public void setK(boolean k) {
+        this.k = k;
+    }
+}
---
+++ src/eu/senla/Task1/Main.java
@@ -0,0 +1,65 @@
+package eu.senla.Task1;
+
+import javax.naming.Name;
+
+public class Main {
+
+  public static byte a;
+  public static short c;
+  public static int d;
+  public static long f;
+  public static float g;
+  public static double h;
+  public static char j;
+  public static boolean k;
+  public static Byte l;
+  public static Short m;
+  public static Integer n;
+  public static Long p;
+  public static Double r;
+  public static Character s;
+  public static Boolean t;
+
+
+    public static void main(String[] args) {
+      byte a = 10;
+      short c = 11;
+      int d = 12;
+      long f= 13;
+      float g = 14;
+      double h = 15;
+      char j = 102;
+      boolean k = true;
+
+      Byte l = 17;
+      Short m = 18;
+      Integer n = 19;
+      Long p = 20l;
+      Float q =2.1f;
+      Double r = 2.2d;
+      Character s =102;
+      Boolean t = false;
+
+      System.out.println(a);
+      System.out.println(c);
+      System.out.println(d);
+      System.out.println(f);
+      System.out.println(g);
+      System.out.println(h);
+      System.out.println(j);
+      System.out.println(k);
+      System.out.println(l);
+      System.out.println(m);
+      System.out.println(n);
+      System.out.println(p);
+      System.out.println(q);
+      System.out.println(r);
+      System.out.println(s);
+      System.out.println(t);
+
+    }
+
+
+
+    }
+
